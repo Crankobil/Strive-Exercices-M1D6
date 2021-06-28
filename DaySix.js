69cabfc9232819d9e23db5088058cc1aa5143d1a
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-06-28 10:10:05 +0100

    Completed up to Exercise 2

--- DaySix.js
+++ DaySix.js
@@ -15,40 +15,60 @@
 /* Ex.A
    Create a variable called "test" and assign a string to it.
 */
+let test = 'string'
 
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
+let sum = 10 + 20
+console.log(sum)
 
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
+let random = Math.floor(Math.random() * 20)
+console.log(random)
 
 /* Ex.D
     Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
-
+let me = {
+    name: 'Christian',
+    surname: 'Ankobil',
+    age: 34
+}
+console.log(me)
 /* Ex.E 
     Programmatically remove the age property from the previously create object.
 */
-
+delete me.age
+console.log(me)
 /* Ex.F 
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
-
+me.myProgLang = ['HTML', 'CSS', 'Javascript'];
+console.log(me)
 /* Ex.G 
    Programmatically remove the last skill from the "skills" array inside the "me" object.
 */
-
+me.myProgLang.pop();
+console.log(me)
 // JS Functions
 /* Ex.1
     Write a function called "dice"; it should randomize an integer number between 1 and 6.
 */
-
+const dice = () => {
+    let resultOfDiceRoll = Math.floor(Math.random() * 6);
+    console.log(resultOfDiceRoll)
+}
+dice ()
 /* Ex.2 
     Write a function called "whoIsBigger" which receives 2 numbers as parameters and returns the biggest one.
 */
-
+const whoIsBigger = (num1,num2) => {
+    num1 > num2 ? console.log(num1) : console.log(num2)
+}
+whoIsBigger(10,204)
 /* Ex.3
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
